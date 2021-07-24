5d0394377060d9d7eb98417fada978f23f961ed6
Israel Barreto <israelprojeto27@gmail.com>
2021-07-23 22:26:00 -0300

    Commit inicial do ms notification

---
+++ ms-housepass-notification/src/main/java/com/housepass/notification/app/MsHousepassNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.housepass.notification.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHousepassNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHousepassNotificationApplication.class, args);
+	}
+
+}
---
+++ ms-housepass-notification/src/test/java/com/housepass/notification/app/MsHousepassNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housepass.notification.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHousepassNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
