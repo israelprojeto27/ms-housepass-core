34953d491d797b47852e5248e147edbdfcec1e4e
Israel Barreto <israelprojeto27@gmail.com>
2021-07-31 23:37:02 -0300

    Ajustes no commit inicial do  MS Message

---
+++ ms-housepass-message/src/test/java/com/housepass/message/app/MsHousepassMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housepass.message.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHousepassMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
